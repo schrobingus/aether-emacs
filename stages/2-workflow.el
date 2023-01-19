30652848769dcb484663f7bb20fe15fd8abac221
BrentBoyMeBob <brent.monning.jr@gmail.com>
2023-01-19 14:55:11 -0800

    bingus-emacs: add mini-modeline minimal configuration, and other updates

--- stages/2-workflow.el
+++ stages/2-workflow.el
@@ -1,3 +1,6 @@
+;; Enable all-the-icons.
+(use-package all-the-icons)
+
 ;; Enable Flycheck mode.
 (use-package flycheck
   :config
@@ -9,10 +12,9 @@
   (add-hook 'prog-mode-hook #'company-mode))
 
 ;; Use LSP.
-(use-package lsp-mode
-  :config
-  (use-package lsp-ui
-    :after lsp))
+(use-package lsp-mode)
+(use-package lsp-ui
+  :after lsp-mode)
 
 ;; Enable Projectile for Emacs.
 (use-package projectile
@@ -39,35 +41,62 @@
 (use-package ivy
   :config
   (straight-use-package 'counsel)
-  (straight-use-package 'swiper)
   (ivy-mode 1)
-  (global-set-key "\C-s" 'swiper)
   (global-set-key (kbd "M-x") 'counsel-M-x)
   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
   (global-set-key (kbd "C-c r")   'counsel-recentf)
   (global-set-key (kbd "C-c b")   'counsel-bookmark)
   (global-set-key (kbd "C-x C-b") 'counsel-switch-buffer))
 
+;; Configure Evil Leader.
+(use-package evil-leader
+  :config
+  (evil-leader/set-leader "Q")
+  (global-evil-leader-mode 1))
+
+;; Add functions to insert lines.
+(defun insert-line-below ()
+  "Insert an empty line below the current line."
+  (interactive)
+  (save-excursion
+    (end-of-line)
+    (open-line 1)))
+(defun insert-line-above ()
+  "Insert an empty line above the current line."
+  (interactive)
+  (save-excursion
+    (end-of-line 0)
+    (open-line 1)))
+
+;; Bind as above, as well as a open-line key.
+(evil-leader/set-key "p" 'open-line)
+(evil-leader/set-key "o" 'insert-line-below)
+(evil-leader/set-key "O" 'insert-line-above)
+
+;; Do the equivalent of noshowmode.
+(setq evil-echo-state nil)
+
 ;; Enable undo-tree as well, hook it to Evil.
 (use-package undo-tree
-   :after evil
-   :config
-   (setq undo-tree-history-directory-alist
-	 '(("." . "~/.emacs.d/temporary/undo"))) ;; Prevent cluttering.
-   (evil-set-undo-system 'undo-tree)
-   (global-undo-tree-mode 1))
+  :after evil
+  :config
+  (setq undo-tree-history-directory-alist
+	'(("." . "~/.emacs.d/temporary/undo"))) ;; Prevent cluttering.
+  (evil-set-undo-system 'undo-tree)
+  (global-undo-tree-mode 1))
 
-;; Enable Keychord, as well as the JJ binding for Evil.
+;; Enable Keychord, as well as several bindings for Evil.
 (use-package key-chord
   :after evil
   :config
   (setq key-chord-two-keys-delay 0.25)
-  (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+  (key-chord-define evil-insert-state-map "jj"
+		    'evil-normal-state) ;; Switch from INSERT to NORMAL.
   (key-chord-mode 1))
 
 ;; On Evil, also set D to the black hole register.
 (defun bb/evil-delete (orig-fn beg end &optional type _ &rest args)
-    (apply orig-fn beg end type ?_ args))
+  (apply orig-fn beg end type ?_ args))
 (advice-add 'evil-delete :around 'bb/evil-delete)
 
 ;; Also enable Evil Commentary.
@@ -135,26 +164,92 @@
 
 ;; Set to NANO theme (Material Light + Nord).
 (straight-use-package 'nano-theme)
-;;(straight-use-package 'nord-theme)
-(require 'nano-theme)
-(load-theme 'nano-dark t)
+(straight-use-package 'nord-theme)
+;;(require 'nano-theme)
+;;(load-theme 'nano-dark t)
 ;;(load-theme 'nano-light t)
-;;(load-theme 'nord t)
+(load-theme 'nord t)
+
+;; Configure mini modeline.
+;;; FORMAT: N | 2-workflow.el | 26:3 W2 E0
+;;; BROKEN: Evil Status, File Name, Line and Character Numbers, Errors + Warnings
+(defun d/flycheck-lighter (state)
+  "Return flycheck information for the given error type STATE. Source: https://git.io/vQKzv"
+  (let* ((counts (flycheck-count-errors flycheck-current-errors))
+         (errorp (flycheck-has-current-errors-p state))
+         (err (or (cdr (assq state counts)) "?"))
+         (running (eq 'running flycheck-last-status-change)))
+    (if (or errorp running) (format " %s " err))))
+(defface modeline-evil-indicator-face
+  `((t :background "#A3BE8C" :foreground "#2E3440" :weight bold))
+  "Section 1 for modeline.")
+(defface modeline-start-section-face
+  `((t :background "#434C5E"))
+  "Major mode and file name face for modeline.")
+(defface modeline-end-section-face
+  `((t :background "#3B4252"))
+  "Line / column number face for modeline.")
+(use-package mini-modeline
+  :init
+  (add-hook 'evil-normal-state-entry-hook
+            (lambda ()
+              (set-face-background 'modeline-evil-indicator-face "#A3BE8C"))) ;; For normal mode.
+  (add-hook 'evil-insert-state-entry-hook
+            (lambda ()
+              (set-face-background 'modeline-evil-indicator-face "#88C0D0"))) ;; For insert mode.
+  (add-hook 'evil-visual-state-entry-hook
+            (lambda ()
+              (set-face-background 'modeline-evil-indicator-face "#81A1C1"))) ;; For visual mode.
+  (add-hook 'evil-replace-state-entry-hook
+            (lambda ()
+              (set-face-background 'modeline-evil-indicator-face "#EBCB8B"))) ;; For replace mode.
+  (setq-default
+   mini-modeline-r-format
+   '("%e" (:eval (propertize evil-mode-line-tag 'face 'modeline-evil-indicator-face))
+     (:eval (propertize (concat " %b ") 'face 'modeline-start-section-face)) ;; %b is buffer name, %m is major mode name.
+     (:eval (propertize (concat " %l:%c ") 'face 'modeline-end-section-face))
+     (:eval
+      (when (and (bound-and-true-p flycheck-mode)
+		 (or flycheck-current-errors
+                     (eq 'running flycheck-last-status-change)))
+	(concat
+	 (cl-loop for state in '((error . "#BF616A")
+				 (warning . "#EBCB8B")
+				 (info . "#A3BE8C"))
+		  as lighter = (d/flycheck-lighter (car state))
+		  when lighter
+		  concat (propertize
+			  lighter
+			  'face `(:foreground ,(cdr state) :background "#3B4252")))
+	 " ")))))
+  (set-face-background 'mode-line "#2E3440")
+  (custom-set-faces
+   '(mini-modeline-mode-line ((t (:background "#4C566A" :box nil :height 0.14))))
+   '(mini-modeline-mode-line-inactive ((t (:background "#434C5E" :box nil :height 0.2)))))
+  :config
+  (mini-modeline-mode t))
+(setq-default evil-mode-line-format '(before . mode-line-front-space))
 
 ;; Utilize frame to edit some settings.
 (setq default-frame-alist
-      (append (list
+	      (append (list
 	       '(width  . 138) ; Default width.
 	       '(height . 48)  ; Default height.
 	       '(vertical-scroll-bars  . nil) ; Disables vertical scroll bars.
-               '(internal-border-width . 16)  ; Adds padding to Emacs (take that StackOverflow!)
+               '(internal-border-width . 8)  ; Adds padding to Emacs.
                '(left-fringe    . 8) ; Add some fringe padding to the left.
-               '(right-fringe   . 8) ; Likewise to the right.
+               '(right-fringe   . 2) ; Likewise to the right.
 	       '(line-spacing   . 0.2)))) ; Finally, add some line spacing.
 (setq linum-format " %d") ; Also add a space at the end of the line number.
 
 ;; Set tab spacing to only 2.
 (setq default-tab-width 2)
+
+;; Enable window divider mode.
+(setq-default window-divider-default-bottom-width 2)
+(setq-default window-divider-default-right-width 2)
+(set-face-foreground 'window-divider "#434C5E")
+(window-divider-mode 1)
 
 ;; Disable Evil in term-specific buffers.
 (add-hook 'term-mode-hook 'evil-local-mode)
--- stages/3-syntax.el
+++ stages/3-syntax.el
@@ -1,10 +1,11 @@
-;; Configure Elisp.
-(add-hook 'emacs-lisp-mode-hook
-	  (lambda () (flycheck-mode -1)))
-
-;; For C / C++.
-(add-hook 'c-mode-hook #'flycheck-mode)
-(add-hook 'c++-mode-hook #'flycheck-mode)
+;; Disable certain Flycheck checkers.
+(setq-default
+ flycheck-disabled-checkers
+ '(
+   c/c++-clang ;; Clang
+   emacs-lisp emacs-lisp-checkdoc ;; Emacs Lisp
+   markdown-markdownlint-cli markdown-mdl ;; Markdown
+   ))
 
 ;; For C#.
 (use-package csharp-mode
@@ -14,64 +15,43 @@
 (use-package dart-mode
   :mode "\\.dart\\'"
   :hook ((dart-mode . lsp)
-	 (dart-mode . flutter-test-mode))
-  :config
-  (use-package flutter
+	 (dart-mode . flutter-test-mode)))
+(use-package flutter
     :after dart-mode
     :bind (:map dart-mode-map
 		("C-M-x" . #'flutter-run-or-hot-reload))
     :custom
-    (flutter-sdk-path "/Users/brent/Sources/flutter")))
+    (flutter-sdk-path "/Users/brent/Sources/flutter"))
 
 ;; For GDScript on Godot.
 (use-package gdscript-mode
   :mode "\\.gd\\'")
 
-;; For Java.
-(add-hook 'java-mode #'flycheck-mode)
-(add-hook 'java-mode #'lsp)
-
-;; For Javascript.
-(add-hook 'js-mode #'flycheck-mode)
-
 ;; For Lua.
 (use-package lua-mode
-  :mode "\\.lua\\'"
-  :hook (lua-mode . flycheck-mode))
+  :mode "\\.lua\\'")
 
 ;; For Markdown.
 (use-package markdown-mode
   :mode "\\.md\\'")
 
-;; For Nix and NixOS.
+; For Nix and NixOS.
 (use-package nix-mode
-  :mode "\\.nix\\'"
-  :hook (nix-mode . flycheck-mode))
+  :mode "\\.nix\\'")
 
 ;; For Processing sketches.
 (use-package processing-mode
   :mode "\\.pde\\'")
 
-;; For Python.
-(add-hook 'python-mode-hook #'flycheck-mode)
-
-;; For Shell scripts.
-(add-hook 'sh-mode-hook #'flycheck-mode)
-
 ;; For Swift.
 (use-package swift-mode
-  :mode "\\.swift\\'"
-  :hook (swift-mode . flycheck-mode))
+  :mode "\\.swift\\'")
 
 ;; For Vim and Vimscript.
 (use-package vimrc-mode
   :mode "\\.vim\\(rc\\)?\\'")
 
-;; For YAML.
+;; For YAML and Ansible.
 (use-package yaml-mode
-  :mode "\\.yml\\'"
-  :config
-  (use-package ansible ; Add Ansible support to the mix.
-    :after yaml-mode
-    :hook (yaml-mode . (lambda () (ansible 1)))
-	   (yaml-mode . flycheck-mode)))
+  :mode "\\.yml\\'")
+
